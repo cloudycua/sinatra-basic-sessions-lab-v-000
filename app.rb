8d798e4be9f28fa4981317a65a5fe4c7b9701e6d
Christine Cua <cloudycua@gmail.com>
2018-07-20 21:28:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
 
   configure do
     enable :sessions
-    set :session.secret, "secret"
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -13,6 +13,21 @@ class App < Sinatra::Base
 
   post '/checkout' do
     @session = session
+  end
+
+  configure do
+    enable :sessions
+    set :session_secret, 'flatiron'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
   end
 
 end
